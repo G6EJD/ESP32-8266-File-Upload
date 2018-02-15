0c2d45207f03021f4d43af6ea47b40cde986846f
G6EJD <d.l.bird@sky.com>
2018-02-15 12:06:07 +0000

    Update Network.h

--- Network.h
+++ Network.h
@@ -1,6 +1,6 @@
 // Adjust the following values to match your needs
 // -----------------------------------------------
-#define   servername "sensorserver2"  // Set your server's logical name here e.g. if 'myserver' then address is http://myserver.local/
+#define   servername "fileserver"  // Set your server's logical name here e.g. if 'myserver' then address is http://myserver.local/
 IPAddress local_IP(192, 168, 0, 150); // Set your server's fixed IP address here
 IPAddress gateway(192, 168, 0, 1);    // Set your network Gateway usually your Router base address
 IPAddress subnet(255, 255, 255, 0);   // Set your network sub-network mask here
